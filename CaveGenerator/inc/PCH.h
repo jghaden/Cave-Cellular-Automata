1567cc97c62719a78d4ee9dd75fc5449d9079573
Joshua Haden <joshghaden@gmail.com>
2020-05-22 12:35:41 -0500

    Updated WriteAt function.

--- CaveGenerator/inc/PCH.h
+++ CaveGenerator/inc/PCH.h
@@ -1,6 +1,9 @@
 #pragma once
 
-#include <iostream>
-#include <Windows.h>
 #include <ctime>
+#include <iostream>
+#include <string>
 #include <vector>
+#include <Windows.h>
+
+#include "System.h"
--- CaveGenerator/inc/System.h
+++ CaveGenerator/inc/System.h
@@ -3,10 +3,12 @@
 #include "PCH.h"
 
 void gotoXY(int x, int y);
-inline void WriteAt(int x, int y, char ch)
+
+template<typename T>
+inline void WriteAt(int x, int y, T data)
 {
 	gotoXY(x, y);
-	std::cout << ch;
+	std::cout << data;
 }
 
 void ShowConsoleCursor(bool showFlag);
